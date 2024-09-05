c25ad5f5a6397e635a9900f39464f46f187c6445
Lachlan Dietrich <lachlan.dietrich@griffithuni.edu.au>
2024-09-05 22:18:13 +1000

    Added/imported bullet related game files

---
+++ Assets/Scripts/NewStandardBullet.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewStandardBullet : MonoBehaviour
+{
+    // BULLET INFORMATION
+    public float speed = 10.0f;
+    public float lifeTime = 5.0f;
+    public int damage = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
