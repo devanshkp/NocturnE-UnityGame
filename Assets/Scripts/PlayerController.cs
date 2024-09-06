2397ff748325dd3b57799ad02f21c2379cfca308
Devansh Kapoor <devansh.kp@outlook.com>
2024-09-06 17:40:59 +1000

    Added textures, played around with terrains, and created a new Player Object.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [Header("References")]
+    public Rigidbody rb;
+    public Transform head;
+    public Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+{
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
