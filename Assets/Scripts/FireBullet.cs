ed949787e6312e0dcb6093e2d58ab630742b1503
Lachlan Dietrich <lachlan.dietrich@griffithuni.edu.au>
2024-09-12 12:51:29 +1000

    Added new bullet prefabs, materials, and scripts for elemental bullets

---
+++ Assets/Scripts/FireBullet.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBullet : MonoBehaviour
+{
+    // BULLET INFORMATION
+    public float speed = 10.0f;
+    public float lifeTime = 5.0f;
+    public int damage = 10;
+
+    // FIRE DAMAGE INFORMATION
+    public float fireTickDamage = 1f;           // damage taken every 'tick'
+    public float fireTickRate = 0.1f;           // The rate each 'tick' of damage occurs in (each x seconds)
+    public int fireLifeTime = 3;                // Total time fire is damaging
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
