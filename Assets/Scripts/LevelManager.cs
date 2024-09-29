204918e87676a4e59288633384839a59189991ba
Lachlan Dietrich <lachlan.dietrich@griffithuni.edu.au>
2024-09-30 00:36:52 +1000

    added the werewolf states and added each state's activation conditions

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -7,6 +7,8 @@ public class LevelManager : MonoBehaviour
     public bool level1DoorOpen = false;
     public bool level2DoorOpen = false;
     public bool level3DoorOpen = false;
+
+    public bool isNightTime = false;
     // Start is called before the first frame update
     void Start()
     {
