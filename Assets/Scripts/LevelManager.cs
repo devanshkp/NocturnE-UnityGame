a80c607f3aa191e5b2f926606d445e3bbdc33e8c
Lachlan Dietrich <lachlan.dietrich@griffithuni.edu.au>
2024-09-29 16:08:44 +1000

    added level manager to keep track of progress

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public bool level1DoorOpen = false;
+    public bool level2DoorOpen = false;
+    public bool level3DoorOpen = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
