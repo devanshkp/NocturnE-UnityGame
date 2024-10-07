ea76b5a73c20a16c10c6d8ebfac8de27f40e4e78
Devansh Kapoor <devanshkp1@gmail.com>
2024-10-07 21:21:34 +1000

    Lock on Icon + Health Bar on enemy - Need to fix the script slightly

---
+++ Assets/Scripts/EnemyUIManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyUIManager : MonoBehaviour
+{
+    public GameObject lockOnIcon;
+    public GameObject healthBar;
+    private Transform cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.transform.Find("EnemyUICamera").GetComponent<Camera>().transform;
+        if (lockOnIcon == null)
+            lockOnIcon =  transform.GetComponentInChildren<Transform>().Find("LockOnIcon").gameObject;
+        if (healthBar != null)
+            healthBar = transform.GetComponentInChildren<Transform>().Find("Health Bar").gameObject; 
+        lockOnIcon.SetActive(false);
+        healthBar.SetActive(false);
+    }
+
+    void LateUpdate()
+    {
+        transform.LookAt(cam.transform);
+        transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f);
+    }
+
+    public void EnableLockOnIcon()
+    {
+        lockOnIcon.SetActive(true);
+    }
+
+    public void DisableLockOnIcon()
+    {
+        lockOnIcon.SetActive(false);
+    }
+
+    public void EnableHealthBar()
+    {
+        healthBar.SetActive(true);
+    }
+
+    public void DisableHealthBar()
+    {
+        healthBar.SetActive(false);
+    }
+}
