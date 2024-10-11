61d98fba4bc930f576c3c21368a58d54fdffa873
Lachlan Dietrich <lachlan.dietrich@griffithuni.edu.au>
2024-10-11 13:10:52 +1000

    added elemental enemies and implemented fire and ice damage functionality

--- Assets/Scripts/Bullet Related Scripts/FireBullet.cs
+++ Assets/Scripts/Bullet Related Scripts/FireBullet.cs
@@ -2,36 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class FireBulletInfo
+{
+    [Header("Fire Settings")]
+    public float fireTickDamage = 0.5f;           // damage taken every 'tick'
+    public float fireTickRate = 0.3f;           // delay per damage tick
+    public int fireLifeTime = 5;                // Total time fire is damaging
+
+    public FireBulletInfo(float fireTickDamage, float fireTickRate,int fireLifeTime)
+    {
+        this.fireTickDamage = fireTickDamage;
+        this.fireTickRate = fireTickRate;
+        this.fireLifeTime = fireLifeTime;
+    }
+}
+
 public class FireBullet : MonoBehaviour
 {
-    // BULLET INFORMATION
-    public float speed = 10.0f;
-    public float lifeTime = 5.0f;
-    public int damage = 10;
-
-    // FIRE DAMAGE INFORMATION
-    public float fireTickDamage = 1f;           // damage taken every 'tick'
-    public float fireTickRate = 0.1f;           // The rate each 'tick' of damage occurs in (each x seconds)
-    public int fireLifeTime = 3;                // Total time fire is damaging
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Fire Settings")]
+    public float fireTickDamage = 0.5f;           // damage taken every 'tick'
+    public float fireTickRate = 0.3f;           // delay per damage tick
+    public int fireLifeTime = 5;                // Total time fire is damaging
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            //  Use a wrapper class to get all the vars
-            other.SendMessage("TakeFireDamage", damage, fireTickDamage, fireTickRate, fireLifeTime);
+            FireBulletInfo damageInfo = new FireBulletInfo(fireTickDamage, fireTickRate, fireLifeTime);
+            other.SendMessage("TakeFireDamage", damageInfo);
         }
     }
 }
